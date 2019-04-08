b13d91a0c123cd6c079a861c197c4b287bb9a8aa
YoungX116 <youngx101x@gmail.com>
2019-04-08 16:11:33 -0700

    changed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 const gulp = require("gulp");
+const watch = require("gulp-watch");
 
 gulp.task("default", () => {
   console.log("Created a gulp task.");
 });
 
-gulp.task("html", () => {
-  console.log("HTML changed.");
+gulp.task("css", () => {
+  console.log("CSS changed.");
+});
+
+gulp.task("watch", () => {
+  watch("./client/src/index.js", () => {
+    gulp.start("css");
+  });
 });
