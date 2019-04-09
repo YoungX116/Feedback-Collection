add858eb4d0a378d7255178572009f603e30b561
YoungX116 <youngx101x@gmail.com>
2019-04-08 17:05:27 -0700

    tested gulp postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 const gulp = require("gulp");
 const watch = require("gulp-watch");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssvars = require("postcss-simple-vars");
+const nested = require("postcss-nested");
 
 gulp.task("default", () => {
   console.log("Created a gulp task.");
 });
 
-gulp.task("css", () => {
-  console.log("CSS changed.");
+gulp.task("styles", () => {
+  console.log("style changed");
+  return gulp
+    .src("./client/public/style.css")
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest("./client/temp"));
 });
 
 gulp.task("watch", () => {
   watch("./client/public/style.css", () => {
-    gulp.start("css");
+    gulp.start("styles");
   });
 });
